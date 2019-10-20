14865986e342100fad5e4e6b9bfed039f55c8cdb
Mateusz Czarnecki <mateuszczrnck@gmail.com>
2019-10-20 11:30:23 +0200

    addBook Library

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -5,11 +5,23 @@ import java.util.List;
 import java.util.Optional;
 
 @Getter
+
 public class Library {
 
-    private List<Book> listBooks = new ArrayList<>();
+    private List<Book> listBook = new ArrayList<>();
     private List<Client> listClient = new ArrayList<>();
     private List<Rental> listRental = new ArrayList<>();
+
+    public void addBook(Book book){
+        Optional<Book> findBook = listBook.stream()
+                .filter(a -> a.getIdBook().equals(book.getIdBook()))
+                .findAny();
+        if (!findBook.isPresent()){
+            listBook.add(book);
+        } else {
+            System.out.println("Taka książka już istnieje!");
+        }
+    }
 
     public void addClient(Client client){
         Optional<Client> findClient = listClient.stream()
