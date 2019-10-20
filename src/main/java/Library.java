b7ddf45605e874273d8ba69a5915fc5631330fce
Mateusz Czarnecki <mateuszczrnck@gmail.com>
2019-10-20 10:29:10 +0200

    add lists to library

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
+
+    private List<Book> listBooks = new ArrayList<>();
+    private List<Client> listClient = new ArrayList<>();
+    private List<Rental> listRental = new ArrayList<>();
 }
