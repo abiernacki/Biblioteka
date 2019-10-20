b064aab5243eeccab27e6a1776a06beaf35d51f7
Adrian Biernacki <adrianbie86@gmail.com>
2019-10-20 12:35:14 +0200

    Library addRental and test

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -5,13 +5,11 @@ import java.util.List;
 import java.util.Optional;
 
 @Getter
-
 public class Library {
 
     private List<Book> listBook = new ArrayList<>();
     private List<Client> listClient = new ArrayList<>();
     private List<Rental> listRental = new ArrayList<>();
-
 
     public void addBook(Book book) {
         Optional<Book> findBook = listBook.stream()
@@ -32,6 +30,24 @@ public class Library {
             listClient.add(client);
         } else {
             System.out.println("Taki klient już istnieje!");
+        }
+    }
+
+
+    public void addRental(Rental rental) {
+        Optional<Rental> foundRental = listRental.stream()
+                .filter(a -> a.getBook().getIdBook().equals(rental.getBook().getIdBook()))
+                .filter(a -> (a.getDateFrom().getTime() >= rental.getDateFrom().getTime()
+                        && a.getDateTo().getTime() <= rental.getDateFrom().getTime())
+                        || (a.getDateFrom().getTime() >= rental.getDateTo().getTime()
+                        && a.getDateTo().getTime() <= rental.getDateTo().getTime())
+                        || (a.getDateFrom().getTime() <= rental.getDateFrom().getTime()
+                        && a.getDateTo().getTime() >= rental.getDateTo().getTime()))
+                .findAny();
+        if (!foundRental.isPresent()){
+            listRental.add(rental);
+        } else {
+            System.out.println("Rezerwacja nie możliwa!");
         }
     }
 
@@ -56,7 +72,6 @@ public class Library {
             listClient.remove(clientExist.get());
         }
     }
-
 
 }
 
