2511ae61808a9dbcb82ec57a37231a2e73324b39
Adrian Biernacki <adrianbie86@gmail.com>
2019-10-20 10:58:28 +0200

    Library addClient()

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,9 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Library {
 
     private List<Book> listBooks = new ArrayList<>();
     private List<Client> listClient = new ArrayList<>();
     private List<Rental> listRental = new ArrayList<>();
+
+    public void addClient(Client client){
+        Optional<Client> findClient = listClient.stream()
+                .filter(a -> a.getIdClient().equals(client.getIdClient()))
+                .findAny();
+        if (!findClient.isPresent()){
+            listClient.add(client);
+        } else {
+            System.out.println("Taki klient już istnieje!");
+        }
+    }
 }
+
