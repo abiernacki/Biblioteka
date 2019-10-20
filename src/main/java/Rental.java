51597715cf242403ac50d7259e74b93cb5066f3a
Adrian Biernacki <adrianbie86@gmail.com>
2019-10-20 10:25:00 +0200

    upgrade Rental

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -1,2 +1,14 @@
+import lombok.*;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Rental {
+
+    private Book book;
+    private Client client;
+    private Date dateTo;
+    private Date dateFrom;
 }
