e24cf821201cf0f4df7bdc7323b70d78ff10f496
Adrian Biernacki <adrianbie86@gmail.com>
2019-10-20 11:23:22 +0200

    LibraryTest add

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -21,4 +21,19 @@ public class LibraryTest {
         // then
         assertEquals(3, listaKsiazek.size());
     }
+
+    @Test
+    public void testAddClient() {
+
+        //given
+        Client client = new Client("Adrian", "Biernacki", 1L, "555555555");
+        Library library = new Library();
+
+        //when
+        library.addClient(client);
+
+        //then
+        assertEquals(1, library.getListClient().size());
+
+    }
 }
